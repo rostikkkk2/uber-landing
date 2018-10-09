7fc986e2b2ae9cf4b5d62aa2add39601f9afa2f3
IRuban <moaner48@gmail.com>
2018-10-09 21:57:03 +0300

    added requirement, fix layout files

---
+++ views/landing/guarantees.php
@@ -0,0 +1,66 @@
+<div class="row">
+  <div class="col-md-12 wrapper-guarantees">
+  <a name="guarantees"></a>
+    <h2 class="text-center mb-50 mt-80">Преимущества</h2>
+    <div class="row">
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_1.png" alt="">
+        </p>
+        <p class="about-service">Заработок от 10 000 грн</p>
+      </div>
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_2.png" alt="">
+        </p>
+        <p class="about-service">Большое количество заказов</p>
+      </div>
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_3.png" alt="">
+        </p>
+        <p class="about-service">Еженедельные выплаты</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_4.png" alt="">
+        </p>
+        <p class="about-service">Свободный график</p>
+      </div>
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_5.png" alt="">
+        </p>
+        <p class="about-service">Бонусы и промо-акции</p>
+      </div>
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_6.png" alt="">
+        </p>
+        <p class="about-service">Удобное приложение</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_7.png" alt="">
+        </p>
+        <p class="about-service">Компенсация отмененной поездки</p>
+      </div>
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_8.png" alt="">
+        </p>
+        <p class="about-service">Программа лояльности</p>
+      </div>
+      <div class="col-md-4 col-sm-4 mt-25">
+        <p class="text-center">
+          <img src="images/guarantees_9.png" alt="">
+        </p>
+        <p class="about-service">Круглосуточная техподдержка</p>
+      </div>
+    </div>
+  </div>
+</div>
