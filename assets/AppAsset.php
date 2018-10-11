c40e58bd16419b69ccfd69e1312050d1fb5d73ca
IRuban <moaner48@gmail.com>
2018-10-12 01:39:50 +0300

    fix modal

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -13,10 +13,10 @@ class AppAsset extends AssetBundle
         'css/font-awesome.min.css'
     ];
     public $js = [
-      'js/script.js'
+        'js/script.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset'
     ];
 }
